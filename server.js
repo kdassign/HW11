e62373c3382530c24e5d22aa802302cfc90722bf
Kelsey Duru <kdassignments@gmail.com>
2021-11-03 22:06:34 -0700

    server.js connects to routes

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require("path");
+const fs = require("fs");
+
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
+require('./routes/routes')(app);
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
