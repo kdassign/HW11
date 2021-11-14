241affcdc799832de49417ea1eee3cadc2de90e1
Kelsey Duru <kdassignments@gmail.com>
2021-11-13 19:26:44 -0800

    fixing stuff

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-require('./routes')(app);
+require('./routes/routes')(app);
 
 
 app.listen(PORT, function () {
