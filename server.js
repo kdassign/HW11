ac95e3c02d837472e33eaa776ad507df321ba858
Kelsey Duru <kdassignments@gmail.com>
2021-11-13 19:54:21 -0800

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,)));
 
 require('./routes/routes')(app);
 
