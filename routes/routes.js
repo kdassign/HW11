9dfb0208166d336c8171f6dc9dffb5c9ccb18e62
Kelsey Duru <kdassignments@gmail.com>
2021-11-13 20:46:07 -0800

    idk atp

--- routes/routes.js
+++ routes/routes.js
@@ -4,44 +4,44 @@ const path = require('path');
 module.exports = app => {
 
     app.get("/api/notes", function (req, res) {
-        noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+        noteData = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
         res.json(noteData);
     });
 
     app.post("/api/notes", function (req, res) {
-        let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        let noteData = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     // gives the current note an id
         req.body.id = noteData.length;
     // pushes the current note to noteData
         noteData.push(req.body);
     //writes right back to db
-        fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
+        fs.writeFileSync("db/db.json", JSON.stringify(noteData));
         console.log("saved to json file: ", req.body);
         res.json(noteData);
     });
 
     app.delete("/api/notes/:id", function (req, res) {
-        let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        let noteData = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
         noteData.splice(req.params.id, 1);
 
-        fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
+        fs.writeFileSync("db/db.json", JSON.stringify(noteData));
         console.log("delete note", req.params.id);
         res.json(noteData);
     });
 
     app.get("/api/notes/:id", function (req, res) {
-        let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        let noteData = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
         var note = req.params.id;
         res.JSON(noteData[req.params.id]);
     })
 
 
     app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
     });
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "./public/index.html"));
     })
 
 }
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,)));
 
-require('./routes/routes')(app);
+require('./routes/routes')(app), "public";
 
 
 app.listen(PORT, function () {
