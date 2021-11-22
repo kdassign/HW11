940e5cdf8c88f8fe53b3bfce28595c0c4232069c
Kelsey Duru <kdassignments@gmail.com>
2021-11-21 18:17:25 -0800

    trying to make some changes to get this to work

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ var express = require("express");
 var path = require("path");
 const fs = require("fs");
 
-
+// Tells node that we are creating an "express" server
 var app = express();
 
+// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,)));
+app.use(express.static(path.join(__dirname)));
 
-require('./routes/routes')(app), "public";
+require('./routes/routes')(app)//,public
+;
 
 
 app.listen(PORT, function () {
